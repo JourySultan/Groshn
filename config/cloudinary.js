de2771b531f641ca102d056dd94368c4e0a67e30
ahmed-91 <ahmad-91>
2025-03-26 04:40:53 +0300

    add cloudinry key

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 
+// Use the CLOUDINARY_URL to configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_url: process.env.CLOUDINARY_URL
 });
 
 module.exports = cloudinary;
