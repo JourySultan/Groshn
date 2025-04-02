87f628b6f9a70bb7c7c19a16453215f7da924723
ahmad-91 <ur.r@hotmail.com>
2025-04-02 18:20:26 +0300

    add adress to users

--- models/User.js
+++ models/User.js
@@ -5,23 +5,54 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true,
+    lowercase: true
   },
   password: {
     type: String,
     required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'customer'],
+    default: 'customer'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  address: {
+    type: String
   }
-}, { timestamps: true });
+});
 
+// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Method to check password validity
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 module.exports = mongoose.model('User', userSchema);
